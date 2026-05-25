30e038c401a6a0397d33d5303ac6533423f43708
Hinokuma Yugo <251018@st.yoshida-g.ac.jp>
2026-05-25 13:50:14 +0900

    no message

--- app/item/update/[id]/page.js
+++ app/item/update/[id]/page.js
@@ -1,93 +1,20 @@
-"use client"
-import { use,useState, useEffect } from "react"
-import { useRouter } from "next/navigation" 
-import useAuth from "../../../utils/useAuth"
-import Header from "../../../components/header"
-import Footer from "../../../components/footer"
-const UpdateItem = ({params}) => {
-    const resolvedParams=use(params)
-    const itemid=resolvedParams.id
-    const [title, setTitle] = useState("")
-    const [price, setPrice] = useState("")
-    const [image, setImage] = useState("")
-    const [description, setDescription] = useState("")
-    const [email, setEmail] = useState("")
-    const [loading,setLoding]=useState(false)
+import MyPage from "./myPage"
 
-    const router = useRouter()
-    const loginUserEmail = useAuth() 
-
-    useEffect(() => {
-        const getSingleItem = async(id) => {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`, {cache: "no-store"})
-            const jsonData = await response.json() 
-            const singleItem = jsonData.singleItem
-            
-            setTitle(singleItem.title)
-            setPrice(singleItem.price)
-            setImage(singleItem.image)
-            setDescription(singleItem.description)
-            setEmail(singleItem.email) 
-            setLoding(true)
-        }  
-        getSingleItem(resolvedParams.id) 
-    },[params]) 
-
-    const handleSubmit = async(e) => {
-        e.preventDefault() 
-        try{
-            const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/update/${resolvedParams.id}`, {
-                method: "PUT",
-                headers: { 
-                    "Accept": "application/json", 
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${localStorage.getItem("token")}`
-                },
-                body: JSON.stringify({
-                    title: title,
-                    price: price,
-                    image: image,
-                    description: description,
-                    email: loginUserEmail    
-                })
-            })
-            const jsonData = await response.json()
-            alert(jsonData.message)  
-            router.push("/") 
-            router.refresh()
-        }catch{
-            alert("アイテム編集失敗") 
-        }
+export async function generateMetadata(context){
+    const singleparams=await context.params
+    const response=await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${singleparams.id}`,{cache:"no-store"})
+    const jsonData=await response.json()
+    const singleItem=jsonData.singleItem
+    return{
+        title:singleItem.title,
+        description:singleItem.description
     }
+}
 
-    if(loading){
-    if(loginUserEmail === email){ 
-        return (
-            <div>
-                <Header/>
-                <h1 className="page-title">アイテム編集</h1>
-                <form onSubmit={handleSubmit}>
-                    <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" name="title" placeholder="アイテム名" required/>
-                    <input value={price} onChange={(e) => setPrice(e.target.value)} type="text" name="price" placeholder="価格" required/>
-                    <input value={image} onChange={(e) => setImage(e.target.value)} type="text" name="image" placeholder="画像" required/>
-                    <textarea value={description} onChange={(e) => setDescription(e.target.value)} name="description" rows={15} placeholder="商品説明" required></textarea>
-                    <button>編集</button>
-                </form>
-               
-            </div>
-        )
-    }else{                            
-        return (
-            <div>
-            <Header/>
-            <h1>権限がありません</h1> 
-            <Footer/>
-            </div>
-            ) 
-    }
-    }else{
-        return <h1>ローディング中</h1>
-    }
+
+
+const UpdateItem=(context)=>{
+    return <MyPage{...context}/>
 }
 
 export default UpdateItem
--- app/user/register/myPage.js
+++ app/user/register/myPage.js
@@ -1,38 +1,61 @@
-import Image from "next/image"  
-import Link from "next/link" 
-import Header from "../../../components/header"
-import Footer from "../../../components/footer"
-const getSingleItem = async(id) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readsingle/${id}`, {cache: "no-store"})
-    const jsonData = await response.json() 
-    const singleItem = jsonData.singleItem
-    return singleItem 
-}  
+"use client"
+import Header from "../../components/header"
+import Footer from "../../components/footer"
 
-const ReadSingleItem = async(context) => {
-    const singleparams=await context.params
-    const singleItem = await getSingleItem(singleparams.id)
-    return (
-        <div className="grid-container-si">
+import { useState } from "react"
+
+export const metadeta={
+    title:"登録ページ",
+    descriptin:"これは登録ページです",
+}
+
+const Register=()=>{
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const handleSubmit= async(e)=>{
+        e.preventDefault()
+        console.log(0)
+        try {
+            const response=await fetch(`"${process.env.NEXT_PUBLIC_URL}/api/user/register`,{
+                method:"POST",
+                headers:{
+                    "Accept":"application/json",
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify({
+                    name:name,
+                    email:email,
+                    password:password
+                })
+            })
+            console.log(1)
+            const jsonData=await response.json()
+            console.log(22)
+            alert(jsonData.message)
+        } catch{
+            alert("ユーザー登録失敗")
+        }
+    }    
+    return(
+        <div>
             <Header/>
-            <title>{singleItem.title}</title>
-            <meta name="description" content={singleItem.description}/>
-            <div>
-                <Image src={singleItem.image} width={750} height={500} alt="item-image" priority/>
-            </div>
-            <div>
-                <h1>{singleItem.title}</h1>
-                <h2>¥{singleItem.price}</h2>
-                <hr/>
-                <p>{singleItem.description}</p>
-                <div>
-                    <Link href={`/item/update/${singleItem._id}`}>アイテム編集</Link>
-                    <Link href={`/item/delete/${singleItem._id}`}>アイテム削除</Link>
-                </div>
-            </div>
+            <h1>ユーザー登録</h1>
+            <form className="page-title" onSubmit={handleSubmit}>
+                <input value={name} onChange={(e)=>{
+                    setName(e.target.value)
+                    }}  type="text" name="name" placeholder="名前" required></input>
+                <input value={email} onChange={(e)=>{
+                    setEmail(e.target.value)
+                    }} type="text" name="email" placeholder="メールアドレス" required></input>
+                <input value={password} onChange={(e)=>{
+                    setPassword(e.target.value)
+                    }} type="text" name="password" placeholder="パスワード" required></input>
+                <button>登録</button>
+            </form>
             <Footer/>
         </div>
     )
 }
 
-export default ReadSingleItem
+export default Register
